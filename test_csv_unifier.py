1139c0232c1f1c2f1641b1d14b2e348706372b90
Nancy Trinh <17187984+nantrinh@users.noreply.github.com>
2020-06-28 13:44:07 -0700

    Add test for nonconforming data

--- test_csv_unifier.py
+++ test_csv_unifier.py
@@ -2,6 +2,8 @@
 Test CSV Unifier
 
 Note that test outputs and stdout are expected to fit in memory.
+
+TODO: test error messages for each
 """
 
 import sys
@@ -10,21 +12,18 @@ from io import StringIO
 import unittest
 
 from main import CSVUnifier
-import ipdb
-
-# TODO: use StringIO instead of actual IO
-
 
 class TestCSVUnifier(unittest.TestCase):
     def setUp(self):
         self.output_file = StringIO() 
         self.batch_size = 2 
-
         self.orig_stdout = sys.stdout 
 
     def written(self):
+        """
+        Helper method: parses the output file and returns a list of rows
+        """
         return [row.split(',') for row in self.output_file.getvalue().strip().split('\n')]
-
 
     def test_all_columns_are_present(self):
         """
@@ -73,10 +72,11 @@ class TestCSVUnifier(unittest.TestCase):
     def test_missing_data(self):
         """
         When the length of a row does not match the length of the header,
-        the row does not get written to the output.
+        an error message is printed, and the row does not get written to the output.
         """
 
-        # the first row is valid, the rest each have a value missing
+        # the first two rows (including the header) are valid,
+        # the rest each have a value missing
         rows = [['Provider Name', 'CampaignID', 'Cost Per Ad Click', 'Redirect Link', 'Phone Number', 'Address', 'Zipcode'],
          ["Auto R' Us", 'AUTO1', '15.00', 'autorus.com/auto1', '8675309', 'Burton Street', '78702'],
          ['AUTO2', '15.00', 'autorus.com/auto1', '8675309', 'Burton Street', '78702'],
@@ -94,40 +94,69 @@ class TestCSVUnifier(unittest.TestCase):
         cu.process(rows)
         cu.clean_up()
 
+#        self.assertEqual('All columns in schema must be present', new_stdout.getvalue().strip())
+#        for i, line in new_stdout.getvalue().strip().split('\n'):
+
         written = self.written() 
         self.assertEqual(2, len(written))
         for i, row in enumerate(written):
             self.assertEqual(rows[i], row)
 
-
-    @unittest.skip('')
     def test_nonconforming_data(self):
         """
         When a row contains data that does not conform to the schema,
         an error is printed and the row does not get written to the output.
         """
-        pass
 
-    @unittest.skip("")
+        # the first two rows (including the header) are valid,
+        # the rest each contain a non-conforming value
+        rows = [['Provider Name', 'CampaignID', 'Cost Per Ad Click', 'Redirect Link', 'Phone Number', 'Address', 'Zipcode'],
+                ["Auto R' Us", 'AUTO1', '15.00', 'autorus.com/auto1', '8675309', 'Burton Street', '78702'],
+                ["   ", 'AUTO2', '15.00', 'autorus.com/auto1', '8675309', 'Burton Street', '78702'],
+                ["Auto R' Us", '123', '15.00', 'autorus.com/auto1', '8675309', 'Burton Street', '78702'],
+                ["Auto R' Us", 'AUTO4', '15.392040', 'autorus.com/auto1', '8675309', 'Burton Street', '78702'],
+                ["Auto R' Us", 'AUTO5', '15.00', 'autorus', '8675309', 'Burton Street', '78702'],
+                ["Auto R' Us", 'AUTO1', '15.00', 'autorus.com/auto1', '123456', 'Burton Street', '78702'],
+                ["Auto R' Us", 'AUTO1', '15.00', 'autorus.com/auto1', '8675309', 'Burton', '78702'],
+                ["Auto R' Us", 'AUTO1', '15.00', 'autorus.com/auto1', '8675309', 'Burton Street', '7870']
+               ]
+
+        sys.stdout = new_stdout = StringIO()
+        cu = CSVUnifier(batch_size=self.batch_size, output_file=self.output_file) 
+        cu.reset_header()
+        cu.process(rows)
+        cu.clean_up()
+
+        written = self.written() 
+        self.assertEqual(2, len(written))
+        for i, row in enumerate(written):
+            self.assertEqual(rows[i], row)
+
     def test_columns_not_in_the_schema(self):
         """
         Columns not listed in the schema are ignored.
+        No error messages are printed.
         """
-        rows_input = [['Provider Name', 'CampaignID', 'AccountId', 'Cost Per Ad Click', 'Redirect Link', 'Phone Number', 'Address', 'Zipcode', 'Favorite Animal'],
+        rows = [['Provider Name', 'CampaignID', 'AccountId', 'Cost Per Ad Click', 'Redirect Link', 'Phone Number', 'Address', 'Zipcode', 'Favorite Animal'],
          ["Auto R' Us", 'AUTO1', '4', '15.00', 'autorus.com/auto1', '8675309', 'Burton Street', '78702', 'Cat']]
 
         rows_expected_output = [['Provider Name', 'CampaignID', 'Cost Per Ad Click', 'Redirect Link', 'Phone Number', 'Address', 'Zipcode'],
          ["Auto R' Us", 'AUTO1', '15.00', 'autorus.com/auto1', '8675309', 'Burton Street', '78702']]
 
-        with open(self.output_filename, 'w', newline='\n') as output_file:
-            cu = CSVUnifier(batch_size=self.batch_size, output_file=output_file) 
-            cu.reset_header()
-            cu.process(rows_input)
-            cu.clean_up()
+        sys.stdout = new_stdout = StringIO()
+        cu = CSVUnifier(batch_size=self.batch_size, output_file=self.output_file) 
+        cu.reset_header()
+        cu.process(rows)
+        cu.clean_up()
 
-        with open(self.output_filename) as output_file:
-            for i, row in enumerate(csv.reader(output_file)):
-                self.assertEqual(rows_expected_output[i], row)
+        written = self.written() 
+        self.assertEqual(len(rows_expected_output), len(written))
+        for i, row in enumerate(written):
+            self.assertEqual(rows_expected_output[i], row)
+
+    @unittest.skip('')
+    def test_columns_are_written_in_the_schema_order(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
